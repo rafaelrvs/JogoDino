d75d1405b5dfc9e073bbbec506e905224f9140b1
Rafael Vieira <rafaelvieira1333@gmail.com>
2023-05-21 13:56:36 -0300

    atualização de funcionalidade
    Adicionando Pontuação

--- script.js
+++ script.js
@@ -1,65 +1,72 @@
-const personagem = document.querySelector('#personagem')
-const quadrado = document.querySelector('#quadrado')
-var btn = document.querySelector('.btn')
-var pontuacao = document.querySelector('#pontos')
+const personagem = document.querySelector("#personagem");
+const quadrado = document.querySelector("#quadrado");
+const quadrado2 = document.querySelector("#quadrado2");
+var btn = document.querySelector(".btn");
+let pontuacao = document.querySelector("#pontuacao");
+let pontos = 0;
 
+const jump = () => {
+  personagem.classList.add("animar");
+  setTimeout(() => {
+    personagem.classList.remove("animar");
+  }, 600);
+};
+window.getComputedStyle(quadrado).getPropertyValue("jump");
+document.addEventListener("keydown", jump);
 
+var testarColisao = setInterval(function () {
+  var LocalizacaoPersonagem = parseInt(
+    window.getComputedStyle(personagem).getPropertyValue("top")
+  );
 
+  var LocalizacaoQuadrado = parseInt(
+    window.getComputedStyle(quadrado).getPropertyValue("left")
+  );
 
+  var LocalizacaoQuadrad2 = parseInt(
+    window.getComputedStyle(quadrado2).getPropertyValue("left")
+  );
 
-btn.style.display= 'none'
-const jump =() => {
-    personagem.classList.add('animar')
-    
-    setTimeout(() =>{
-        personagem.classList.remove('animar')
-    },600)
-    
-    
+  if (
+    (LocalizacaoQuadrado < 20 &&LocalizacaoQuadrado > 0 && LocalizacaoPersonagem >= 300) ||
+    LocalizacaoPersonagem >= 300 && LocalizacaoQuadrad2 < 20&& LocalizacaoQuadrad2 >0 ||LocalizacaoQuadrado < 20 &&LocalizacaoQuadrado > 0 && LocalizacaoPersonagem >= 280){
+    perdeu();
+  }
+  else {
+
+            pontos = pontos+1
+            pontuacao.innerHTML = pontos
+            console.log(pontos)
+
+            if(pontos>1000){
+            fase2()
+            }
+          }
+}, 10);
+
+function perdeu() {
+  quadrado.style.animation = "none";
+  personagem.style.display = "none";
+  quadrado2.style.display = "none";
+  var texto = document.getElementById("texto");
+  var texto2 = document.getElementById("texto2");
+  texto2.innerHTML = "x";
+  pontuacao.style.display = "none";
+  texto.innerHTML = "Você perdeu";
+  texto.style.display = "grid";
+  btn.style.display = "grid";
+
+  if (btn == true) {
+    atualizacao();
+  }
 }
-window.getComputedStyle(quadrado).getPropertyValue('jump')
-document.addEventListener('keydown', jump)
-
-var testarColisao = setInterval(function(){
-    var LocalizacaoPersonagem= parseInt(
-        window.getComputedStyle(personagem).getPropertyValue('top')
-    )
-    var LocalizacaoQuadrado= parseInt(
-        window.getComputedStyle(quadrado).getPropertyValue('left')
-    )
-
-    if(LocalizacaoQuadrado<7&& LocalizacaoQuadrado > 0 && LocalizacaoPersonagem >=300 || LocalizacaoQuadrado<7&& LocalizacaoQuadrado > 0 && LocalizacaoPersonagem >= 285 ){
-        perdeu()
-        
-    }
-},10)
-
-function perdeu(){
-    quadrado.style.animation = 'none'
-    personagem.style.display = 'none'
-
-    var texto = document.getElementById('texto')
-    var texto2 = document.getElementById('texto2').innerHTML="x"
-    pontuacao.style.display='none'
-    pontuacao.innerHTML=""
-    texto.innerHTML ='Você perdeu'
-    texto.style.display='grid'
-    var btn = document.querySelector('.btn')
-    btn.style.display = 'flex'
-    if(btn == true){
-        atualizacao()
-
-    }
+function atualizacao() {
+  setTimeout(function () {
+    // Atualize a página
+    window.location.reload();
+  }, 5000);
 }
-function atualizacao(){
-    setTimeout(function() {
-        // Atualize a página
-        window.location.reload()
-      }, 5000)
-    }
-function score (){
-    var pontuacao = document.getElementById('.pontuacao').innerHTML = 'teste'
-    
+function fase2(){
+    quadrado2.style.display='grid' 
+
 }
-
-
